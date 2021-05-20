47a132020c6f09c2c8c594b987e4ee0fb53e2382
ferreiraandy <andyferreira92@gmail.com>
2021-05-20 20:06:48 -0300

    Big bang

---
+++ spec/lalamove/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Lalamove::Ruby do
+  it "has a version number" do
+    expect(Lalamove::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
