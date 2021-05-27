04ec2a4e4cbbd76a9ae5a3916b2839fe2e8a2b67
ferreiraandy <andyferreira92@gmail.com>
2021-05-27 19:47:27 -0300

    Changes simplecov formatter

--- spec/support/configs/simple_cov_config.rb
+++ spec/support/configs/simple_cov_config.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov_json_formatter'
 
 module SimpleCovConfig
   def self.configure
-    SimpleCov.minimum_coverage 60
+    SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+    SimpleCov.minimum_coverage 90
     SimpleCov.start do
       add_filter { |source_file| cover?(source_file.lines) }
       add_filter '/spec/'
