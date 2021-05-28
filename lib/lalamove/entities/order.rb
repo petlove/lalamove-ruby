7586efe7b1ca4b77345130248328a2138c66ca70
mauricionr <mauricioskateboard@gmail.com>
2021-05-28 09:18:34 -0300

    handle optional

--- lib/lalamove/entities/order.rb
+++ lib/lalamove/entities/order.rb
@@ -3,9 +3,9 @@
 module Lalamove
   module Entities
     class Order < Lalamove::Entities::Quotation
-      attribute :sms, Types::Strict::Bool
-      attribute :pod, Types::Strict::Bool
-      attribute :fleetOption, Types::String.optional
+      attribute :fleetOption, Types::String.default('FLEET_ALL')
+      attribute :sms, Types::Strict::Bool.default(true)
+      attribute :pod, Types::Strict::Bool.default(false)
       attribute :quotedTotalFee, QuotedTotalFee
     end
   end
--- spec/lalamove/entities/order_spec.rb
+++ spec/lalamove/entities/order_spec.rb
@@ -3,56 +3,104 @@
 require 'spec_helper'
 
 RSpec.describe Lalamove::Entities::Order do
-  it 'is inherited' do
+  it 'is inherited from order entity' do
     expect(described_class).to be < Lalamove::Entities::Quotation
-  end
-
-  let(:attributes) do
-    {
-      scheduleAt: '2020-09-01T14:30:00.00Z',
-      serviceType: 'MOTORCYCLE',
-      stops: [
-        {
-          location: {
-            lat: '13.740167',
-            lng: '100.535237'
-          },
-          addresses: {
-            pt_BR: {
-              displayString: 'Rua Dom Salomão Ferraz, 123',
-              country: 'TH'
-            }
-          }
-        }
-      ],
-      deliveries: [
-        {
-          toStop: 1,
-          toContact: {
-            name: 'Person',
-            phone: '5511999999999'
-          },
-          remarks: 'ORDER#94\r\n1. Tshirt จำนวน 1\r\n2. Hoodie จำนวน 1\r\n'
-        }
-      ],
-      requesterContact: {
-        name: 'Person',
-        phone: '5511999999999'
-      },
-      specialRequests: ['COD'],
-      fleetOption: 'FLEET_FIRST',
-      sms: false,
-      pod: false,
-      quotedTotalFee: {
-        amount: '108000',
-        currency: 'THB'
-      }
-    }
   end
 
   subject { described_class.new(attributes) }
 
-  it_behaves_like 'entity_attributes', %i[
-    scheduleAt serviceType stops deliveries requesterContact specialRequests quotedTotalFee sms pod fleetOption
-  ]
+  context 'including optional order attributes' do
+    let(:attributes) do
+      {
+        scheduleAt: '2020-09-01T14:30:00.00Z',
+        serviceType: 'MOTORCYCLE',
+        stops: [
+          {
+            location: {
+              lat: '13.740167',
+              lng: '100.535237'
+            },
+            addresses: {
+              pt_BR: {
+                displayString: 'Rua Dom Salomão Ferraz, 123',
+                country: 'TH'
+              }
+            }
+          }
+        ],
+        deliveries: [
+          {
+            toStop: 1,
+            toContact: {
+              name: 'Person',
+              phone: '5511999999999'
+            },
+            remarks: 'ORDER#94\r\n1. Tshirt จำนวน 1\r\n2. Hoodie จำนวน 1\r\n'
+          }
+        ],
+        requesterContact: {
+          name: 'Person',
+          phone: '5511999999999'
+        },
+        specialRequests: ['COD'],
+        fleetOption: 'FLEET_FIRST',
+        sms: true,
+        pod: true,
+        quotedTotalFee: {
+          amount: '108000',
+          currency: 'THB'
+        }
+      }
+    end
+
+    it_behaves_like 'entity_attributes', %i[
+      scheduleAt serviceType stops deliveries requesterContact specialRequests quotedTotalFee sms pod fleetOption
+    ]
+  end
+
+  context 'without optional attributes' do
+    let(:attributes) do
+      {
+        scheduleAt: '2020-09-01T14:30:00.00Z',
+        serviceType: 'MOTORCYCLE',
+        stops: [
+          {
+            location: {
+              lat: '13.740167',
+              lng: '100.535237'
+            },
+            addresses: {
+              pt_BR: {
+                displayString: 'Rua Dom Salomão Ferraz, 123',
+                country: 'TH'
+              }
+            }
+          }
+        ],
+        deliveries: [
+          {
+            toStop: 1,
+            toContact: {
+              name: 'Person',
+              phone: '5511999999999'
+            },
+            remarks: 'ORDER#94\r\n1. Tshirt จำนวน 1\r\n2. Hoodie จำนวน 1\r\n'
+          }
+        ],
+        requesterContact: {
+          name: 'Person',
+          phone: '5511999999999'
+        },
+        specialRequests: ['COD'],
+        quotedTotalFee: {
+          amount: '108000',
+          currency: 'THB'
+        }
+      }
+    end
+
+    it_behaves_like 'entity_attributes', %i[
+      scheduleAt serviceType stops deliveries requesterContact specialRequests quotedTotalFee fleetOption pod sms
+    ]
+  end
 end
