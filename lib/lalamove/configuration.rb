1cdbd9ff15d155e0a9495acaae344833b5477d1f
Anderson Ferreira <andyferreira92@gmail.com>
2021-06-10 10:53:42 -0300

    Changes environment (#24)
    Changes environment

--- lib/lalamove/configuration.rb
+++ lib/lalamove/configuration.rb
@@ -5,7 +5,7 @@ module Lalamove
     attr_accessor :host, :production, :secret, :token, :currency, :country, :city
 
     PRODUCTION = 'https://rest.lalamove.com'
-    SANDBOX    = 'https://sandbox-rest.lalamove.com'
+    SANDBOX    = 'https://rest.sandbox.lalamove.com'
 
     def initialize
       @host = production ? PRODUCTION : SANDBOX
